93b21ebfa25cb44bad5ed9acb93987bdeaaba0c3
Marcus Nyeholt <marcus@symbiote.com.au>
2018-12-05 20:38:11 +1100

    new(editor) Added baseline editor package

---
+++ editor/webpack.config.js
@@ -0,0 +1,72 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    entry: {
+        main: [
+            `./src/index.js`,
+            `./style/index.scss`,
+        ],
+        // 3rd party libraries
+        // vendor: [
+        // ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            minimize: {
+                                safe: true
+                            }
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            autoprefixer: {
+                                browsers: ["last 2 versions"]
+                            }
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {}
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
