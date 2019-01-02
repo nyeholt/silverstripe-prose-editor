7deca087f4d73bd7963ac0526b2e14c44e2a607b
Marcus Nyeholt <marcus@symbiote.com.au>
2019-01-02 19:09:40 +1100

    new(image-paste) Chrome support for pasting in a new image with a data: url src

--- editor/src/plugins/image-paste.js
+++ editor/src/plugins/image-paste.js
@@ -19,7 +19,47 @@ export const imagePaste = new Plugin({
                 for (var i = 0; i < clipboardData.types.length; i++) {
                     if (clipboardData.types[i].match(IMAGE_TYPE) || clipboardData.items[i].type.match(IMAGE_TYPE)) {
                         // by simply returning true, we let the image get pasted as base64 data: urls.
-                        return true;
+                        var file = clipboardData.items[i].getAsFile();
+                        if (!file) {
+
+                            file = clipboardData.items[i].getAsString(function (s) {
+                                console.log(s);
+                            });
+                            alert("Could not convert clipboard data to file, please try a smaller image");
+                            continue;
+                        }
+                        var reader = new FileReader();
+
+                        reader.onload = function (evt) {
+                            if (evt.target.result && evt.target.result.length > 0 && evt.target.result.length < MAX_PASTE_SIZE) {
+
+                                // declared here, otherwise the paste action can change the focused element before the post responds
+                                PASTING = true;
+
+                                if (view.state) {
+                                    let sel = view.state.selection;
+                                    const imageNodeType = view.state.config.schema.nodes.image;
+                                    if (imageNodeType && sel.$from.parent.canReplaceWith(sel.$from.index(), sel.$from.index(), imageNodeType)) {
+                                        const imgProps = {
+                                            src: evt.target.result
+                                        }
+                                        var img = imageNodeType.create(imgProps);
+
+                                        view.dispatch(view.state.tr.replaceSelectionWith(img));
+                                    }
+                                }
+
+                                // $.post(UPLOAD_ENDPOINT, {SecurityID: secId, rawData: evt.target.result}).then(function (res) {
+                                //     PASTING = false;
+                                //     if (res && res.success) {
+                                //         updateModel.setContent(updateDirective, {url: res.url});
+                                //     }
+                                // }).done(function (done) {
+                                //     PASTING = false;
+                                // })
+                            }
+                        };
+                        reader.readAsDataURL(file);
                     }
                 }
             }
