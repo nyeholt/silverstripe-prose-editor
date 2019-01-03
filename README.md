87985377c7e10bac6bfe97233ecde3fe49c7b7ac
Marcus Nyeholt <marcus@symbiote.com.au>
2019-01-03 22:19:03 +1100

    new(ProseShortcodes) Added shortcodes to module

--- README.md
+++ README.md
@@ -7,9 +7,9 @@
 [![Total Downloads](https://poser.pugx.org/symbiote/silverstripe-prose-editor/downloads.svg)](https://packagist.org/packages/symbiote/silverstripe-prose-editor)
 [![License](https://poser.pugx.org/symbiote/silverstripe-prose-editor/license.svg)](https://github.com/symbiote/silverstripe-prose-editor/blob/master/LICENSE.md)
 
-[Short Description, 2-4 sentences]
+Provides a wysiwyg editor for HTMLText content fields. 
 
-![TODO_CHANGE_THIS](docs/images/main.png)
+Combined with the frontend authoring module, provides in-place page editing capability
 
 ## Composer Install
 
@@ -22,6 +22,35 @@ composer require symbiote/silverstripe-prose-editor:~1.0
 * SilverStripe 4.1+
 
 ## Documentation
+
+* Markdown-like shortcuts for formatting and creating lists, eg strike, bold, inline code
+* Table editing 
+* Image paste and upload, 
+* Insert images from the CMS
+* Insert page links from the CMS
+* Creating new pages using square brackets/round brackets, [{page name}]({url-segment}]. You can leave url-segment empty, and one will be automatically created
+
+### Shortcodes
+
+Add the following to your _config.php if you'd like some additional shortcodes made available
+
+```
+ShortcodeParser::get('default')->register('show_field', array(ProseShortcodes::class, 'show_field_shortcode'));
+ShortcodeParser::get('default')->register('listing', array(ProseShortcodes::class, 'listing_content'));
+ShortcodeParser::get('default')->register('workflow_tasks', array(ProseShortcodes::class, 'workflow_tasks'));
+ShortcodeParser::get('default')->register('random_item', array(ProseShortcodes::class, 'random_item'));
+ShortcodeParser::get('default')->register('userform', array(ProseShortcodes::class, 'userform'));
+```
+
+* show_field(field, args) : Displays a field from the 'context' object, typically the currently viewed page. 
+  * field: The field to display; can be in dotted notation for relationship traversal, ie Team.Title
+  * args: Comma separated list of arguments to pass when resolving the value
+* listing(page_id, source_id) : Requires the ListingPage module, lists out content
+  * page_id: The listing page to render
+  * source_id: The source of the listing. Pass in 'me' to refer to the current page
+* workflow_tasks: Lists out the current user's workflow tasks
+* random_item: Gets a random page from the last 50 edited pages 
+* userform(form_id): Display the content of the userform from ID form_id
 
 ### Quick Start
 
---
+++ src/ProseShortcodes.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Symbiote\Prose;
+
+use \Page;
+
+use SilverStripe\ORM\DataObject;
+use Symbiote\ListingPage\ListingPage;
+use SilverStripe\Control\Controller;
+use Symbiote\AdvancedWorkflow\DataObjects\WorkflowInstance;
+use SilverStripe\Security\Member;
+use SilverStripe\ORM\ArrayList;
+use SilverStripe\Core\Convert;
+use Symbiote\ListingPage\ListingTemplate;
+use SilverStripe\View\SSViewer;
+use SilverStripe\CMS\Controllers\ContentController;
+
+
+
+class ProseShortcodes
+{
+    public static function childlist_handler($arguments, $content = null, $parser = null)
+    {
+        $page = self::shortcode_object($arguments);
+        if ($page) {
+            return $page->renderWith('ListingPage_ChildListing');
+        }
+    }
+    public static function show_field_shortcode($arguments, $content = null, $parser = null)
+    {
+        $page = self::shortcode_object($arguments);
+        if (!$page) {
+            return '';
+        }
+        $field = isset($arguments['field']) ? $arguments['field'] : 'Title';
+        $extraArgs = isset($arguments['args']) ? explode(',', $arguments['args']) : [];
+        return self::field_value($page, $field, $extraArgs);
+    }
+    private static function field_value($object, $field, $extraArgs)
+    {
+        $bits = explode('.', $field);
+        $nextField = array_shift($bits);
+        if (count($bits) === 0) {
+            if (!($object instanceof DataObject) && method_exists($object, $nextField)) {
+                return call_user_func_array(array($object, $nextField), $extraArgs);
+            }
+            return $object->$nextField; //getField($nextField);;
+        }
+        $nextObject = $object->dbObject($nextField);
+        return self::field_value($nextObject, implode('.', $bits), $extraArgs);
+    }
+    private static function shortcode_object($arguments)
+    {
+        $page = null;
+        $class = isset($arguments['class']) ? $arguments['class'] : 'Page';
+        if (isset($arguments['id'])) {
+            $page = $class::get()->byID($arguments['id']);
+        }
+        if (!$page) {
+            $controller = Controller::has_curr() ? Controller::curr() : null;
+            $page = $controller instanceof ContentController ? $controller->data() : null;
+        }
+        return $page && $page->hasMethod('canView') ? ($page->canView() ? $page : null) : $page;
+    }
+
+    public static function listing_content($arguments, $content = null, $parser = null)
+    {
+        $pageId = isset($arguments['page_id']) ? $arguments['page_id'] : 0;
+        $sourceId = isset($arguments['source_id']) ? $arguments['source_id'] : 0;
+        if (!$pageId) {
+            return "Please set a page_id attribute of the listing page to embed, and optionally a source_id for it to list from ('me' is valid here)";
+        }
+        $listingPage = ListingPage::get()->byId($pageId);
+        if ($listingPage) {
+            if ($sourceId) {
+                if ($sourceId === 'me' && Controller::has_curr()) {
+                    $ctrl = Controller::curr();
+                    if ($ctrl instanceof ContentController) {
+                        $sourceId = $ctrl->data()->ID;
+                    }
+                }
+                $listingPage->ListingSourceID = (int)$sourceId;
+            }
+            return $listingPage->Content();
+        }
+    }
+    public static function workflow_tasks($arguments, $content = null, $parser = null)
+    {
+        $currentUser = Member::currentUser();
+        if (!$currentUser) {
+            return '';
+        }
+        $groups = $currentUser->Groups()->column();
+        $groupInstances = WorkflowInstance::get()->filter([
+            'Groups.ID' => $groups,
+            'WorkflowStatus:not' => 'Complete',
+        ])->toArray();
+        $userInstances = WorkflowInstance::get()->filter([
+            'Users.ID' => $currentUser->ID,
+            'WorkflowStatus:not' => 'Complete',
+        ])->toArray();
+        $instances = ArrayList::create(array_merge($groupInstances, $userInstances));
+        if (isset($arguments['template'])) {
+            return self::process_shortcode_template($arguments['template'], $instances);
+        }
+        $map = $instances->map();
+        $items = [];
+        foreach ($instances as $item) {
+            $target = $item->getTarget();
+            if ($target instanceof CMSPreviewable) {
+                $result['CMSLink'] = $target->CMSEditLink();
+            }
+            $link = $target && $target instanceof CMSPreviewable ? $target->CMSEditLink() :
+                "admin/workflows/WorkflowDefinition";
+            $items[] = '<li><a href="' . $link . '">' . Convert::raw2xml($item->Title) . '</a></li>';
+        }
+        return '<ul>' . implode($items) . '</ul>';
+    }
+    public static function process_shortcode_template($templateId, $items)
+    {
+        if ($templateId) {
+            $listing = ListingTemplate::get()->filter('Title', $templateId)->first();
+            if ($listing) {
+                $item = ArrayData::create(array('Items' => $items));
+                $view = SSViewer::fromString($listing->ItemTemplate);
+                return $view->process($item);
+            }
+        }
+    }
+    public static function random_item($arguments, $content = null, $parser = null)
+    {
+        $items = Page::get()->sort('LastEdited DESC')->limit(50)->column();
+        $key = array_rand($items);
+        $page = Page::get()->byID($items[$key]);
+        return $page->renderWith('RandomItem');
+    }
+    public static function userform($arguments, $content = null, $parser = null)
+    {
+        $formId = isset($arguments['form_id']) ? $arguments['form_id'] : null;
+        if (!$formId) {
+            return "Please set form_id";
+        }
+        $form = UserDefinedForm::get()->byID($formId);
+        if (!$form) {
+            return "Form $formId not found";
+        }
+        $controller = ModelAsController::controller_for($form);
+        $controller->init();
+        $form = $controller->Form();
+        return $form ? $form->forTemplate() : 'Form not configured';
+    }
+
+}
