51f4d65662e6f0c0dd38fb27066777474dd42258
Marcus Nyeholt <marcus@symbiote.com.au>
2019-11-10 12:18:28 +1100

    new(TextField) Added option to change the type of the text field (ie email, color)

--- editor/src/fields/TextField.js
+++ editor/src/fields/TextField.js
@@ -1,9 +1,11 @@
 import { Field } from "./Field";
 
 export class TextField extends Field {
+    textType = "text";
+
     render() {
         var input = document.createElement("input");
-        input.type = "text";
+        input.type = this.textType;
         input.name = this.options.name ? this.options.name : this.options.label;
         input.placeholder = this.options.placeholder ? this.options.placeholder : '...';
         input.value = this.options.value || "";
