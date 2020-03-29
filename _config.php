6f9873d4283811d57a7fcdf8198ae92d4ecb430c
Marcus Nyeholt <marcus@symbiote.com.au>
2020-03-29 12:56:29 +1100

    fix(shortcodes) Set config for shortcodes referenced in editor

--- _config.php
+++ _config.php
@@ -5,4 +5,7 @@ use Symbiote\Prose\ProseShortcodes;
 
 ShortcodeParser::get('default')->register('inline_placeholder', array(ProseShortcodes::class, 'placeholder'));
 ShortcodeParser::get('default')->register('block_placeholder', array(ProseShortcodes::class, 'block_placeholder'));
+
 ShortcodeParser::get()->register('embed', [ProseShortcodes::class, 'embed_shortcode']);
+ShortcodeParser::get()->register('listing', [ProseShortcodes::class, 'listing_content']);
+ShortcodeParser::get()->register('show_field', [ProseShortcodes::class, 'show_field_shortcode']);
