b9692073c4f60e14d2bd0ada27ca5592428d2058
Marcus Nyeholt <marcus@symbiote.com.au>
2020-04-11 16:53:56 +1000

    fix(schema) Adjusted declaration of image overwrite

--- editor/src/schema.js
+++ editor/src/schema.js
@@ -23,36 +23,39 @@ schemaNodes = schemaNodes.append(tNodes);
 schemaNodes = schemaNodes.append({
     inline_shortcode: InlineShortcodeNodeSpec,
     block_shortcode: BlockShortcodeNodeSpec,
-    image: {
-        inline: true,
-        attrs: {
-            src: {},
-            alt: { default: null },
-            title: { default: null },
-            width: { default: null },
-            height: { default: null },
-            // support for silverstripe specific attrs
-            'data-id': { default: null },
-            'data-shortcode': { default: null },
-        },
-        group: "inline",
-        draggable: true,
-        parseDOM: [{
-            tag: "img[src]", getAttrs: function getAttrs(dom) {
-                return {
-                    src: dom.getAttribute("src"),
-                    title: dom.getAttribute("title"),
-                    alt: dom.getAttribute("alt"),
-                    width: dom.getAttribute('width'),
-                    height: dom.getAttribute('height'),
-                    'data-id': dom.getAttribute('data-id'),
-                    'data-shortcode': dom.getAttribute('data-shortcode')
-                }
-            }
-        }],
-        toDOM: function toDOM(node) { return ["img", node.attrs] }
-    }
+
 })
+
+schemaNodes = schemaNodes.update('image', {
+    inline: true,
+    attrs: {
+        src: {},
+        alt: { default: null },
+        title: { default: null },
+        width: { default: null },
+        height: { default: null },
+        // support for silverstripe specific attrs
+        'data-id': { default: null },
+        'data-shortcode': { default: null },
+    },
+    group: "inline",
+    draggable: true,
+    parseDOM: [{
+        tag: "img[src]", getAttrs: function getAttrs(dom) {
+            return {
+                src: dom.getAttribute("src"),
+                title: dom.getAttribute("title"),
+                alt: dom.getAttribute("alt"),
+                width: dom.getAttribute('width'),
+                height: dom.getAttribute('height'),
+                'data-id': dom.getAttribute('data-id'),
+                'data-shortcode': dom.getAttribute('data-shortcode')
+            }
+        }
+    }],
+    toDOM: function toDOM(node) { return ["img", node.attrs] }
+});
+
 // custom link insert thing.
 const schemaMarks = schema.spec.marks.append({
     strike: {
