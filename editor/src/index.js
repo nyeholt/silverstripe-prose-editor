f903771e3df1958964298a01cd42d7fe698299fc
Marcus Nyeholt <marcus@symbiote.com.au>
2020-06-18 10:05:14 +1000

    fix(index.js) Set initialisation to occur onload rather than immediately

--- editor/src/index.js
+++ editor/src/index.js
@@ -1,23 +1,22 @@
 
 import { setupEditor } from "./setup"
 
+window.addEventListener('load', function () {
+    const editors = [];
 
-const editors = [];
+    const editorNodes = document.querySelectorAll('div.ProseEditorField');
 
-const editorNodes = document.querySelectorAll('div.ProseEditorField');
+    for (let i = 0; i < editorNodes.length; i++) {
+        let node = editorNodes[i];
 
-for (let i = 0; i < editorNodes.length; i++) {
-    let node = editorNodes[i];
+        let editor = function (node) {
+            const editorNode = node.getElementsByClassName('ProseEditorFieldEditor')[0];
+            const editorValue = node.getElementsByClassName('ProseEditorFieldValue')[0];
+            let editorStore = node.getElementsByClassName('ProseEditorFieldStorage')[0];
 
-    let editor = function (node) {
-        const editorNode = node.getElementsByClassName('ProseEditorFieldEditor')[0];
-        const editorValue = node.getElementsByClassName('ProseEditorFieldValue')[0];
-        let editorStore = node.getElementsByClassName('ProseEditorFieldStorage')[0];
+            return setupEditor(editorNode, editorValue, editorStore);
+        }(node);
 
-        return setupEditor(editorNode, editorValue, editorStore);
-    }(node);
-
-    editors.push(editor);
-}
-
-
+        editors.push(editor);
+    }
+})
